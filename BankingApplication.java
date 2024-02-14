4af83682731a56eb4a5ded57ffbc6b2e581b14d7
Naveenkumar <naveent1508@gmail.com>
2024-02-14 11:42:49 +0530

    BankApplication changes Done

---
+++ BankingApplication.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class BankingApplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BankAccount bank1 = new BankAccount("Naveen", "012");
+        bank1.showMenu();
+
+    }
+
+}
